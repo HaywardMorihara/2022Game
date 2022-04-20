5b1007e456760f91722fd0a9ab8887d68220e239
HaywardMorihara <nathanielmorihara@gmail.com>
2022-04-20 08:53:50 -0400

    Splash Screen

---
+++ ui/menus/SplashScreen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+export var wait_time := 2.5
+
+var progress := 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	progress += delta;
+	var progress_percent := progress / wait_time;
+	print(progress_percent);
+	$ProgressBar.set_value(progress_percent * 100);
+	if progress_percent >= 1.0:
+		get_tree().change_scene("res://ui/menus/MainMenu.tscn");
